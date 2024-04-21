526b94f63617b3d9d1ed323cf16991c5750dc96a
Adam Reece <adam@reece.wales>
2024-04-21 20:56:10 +0100

    QoL improvements
    Return exit code 1 instead of 0 when run without administrative privileges.
    Added configuration options:
    - UDP listener IP address and port number. (Instead of "127.0.0.1" and 8008.)
    - Custom firewall block command.
    - Windows: Group blocked IPs into a single firewall rule.
    Corrected escape sequences in log RegEx matchers.
    General polishing up.
    
    This still requires further testing! Also QoL more to come.

--- README.md
+++ README.md
@@ -1,44 +1,110 @@
 # GSDDDOSS (GoldSource Denied Distributed Denial of Service Script)
-GSDDDOSS is a simple python script that monitors your goldsrc/svengine game server's udp log output for the following attacks:
-Bad Rcon,
-Split Packets,
-and A2S abuse / Reflected DDoS
 
-Any ip associated with any of these attacks will be automagically blocked in the firewall.
+GSDDDOSS is a simple Python script that monitors your GoldSrc/Svengine game server's UDP log output for the following attacks:
 
-(blame H2 for the name :P)
+* Bad RCON attempts
+* Split packets
+* A2S abuse / Reflected DDoS
+
+Any IP address associated with any of these attacks will be automagically blocked in your system's firewall.
+
+(Blame H2 for the name 😛)
 
 If you find a bug, or have improved on the script - feel free to make a pull request!
 
-## Requirements:
-python 3.6 or greater.
+## Requirements
 
-## Support:
-Windows (tested)
+* Python 3.6 or greater.
+* Administrator / super user privileges: This is necessary to block IP addresses with your system's firewall.
 
-Linux (untested, but should work)
+## Support
 
-## Installation:
-Download & run the script, it will automatically start listening on udp port 8008.
-In your server.cfg file add the line: ```logaddress_add 127.0.0.1 8008```
+* Windows (tested)
+* Linux (untested, but should work)
 
-and either restart the server, or rcon the same line in the server console.
+## Configuration
 
-note: ideally you wanna run the python command in a loop in case it crashes or something, use screen or tmux on linux.
+You can optionally define a configuration as follows as file "config.json". If it does not exist a default configuration will be created.
 
-## Windows .bat trick for lazy administrator:
-(this assumes python3 is added in PATH)
+### `listener_addr`
 
-Make a new .bat file containing:
+Default: "127.0.0.1"
+
+IP address for the log receiver to listen on. Specify "0.0.0.0" to listen on all IP addresses on the system.
+
+If this tool is running on a different system to your game server you will need to configure relevant firewalls so that UDP traffic going in to the listener address/port is permitted.
+
+**Always restrict this exclusively to the IP addresses of your game servers.** If you open this up to the world anyone can (fraudulently) trigger an IP address block without your consent.
+
+### `listener_port`
+
+Default: 8008
+
+UDP port for log receiver to listen on.
+
+(See firewall remark above.)
+
+### `command_add_block`
+
+Default: *(empty)*
+
+Custom system command to run when blocking an IP address in your firewall.
+
+* Include a placeholder "`{ip}`" for where the IP address should appear in your command.
+* Leave empty/undefined to use the platform default:
+  * On Windows this is: `netsh advfirewall firewall add rule name=\"Blocked IP\" dir=in interface=any action=block remoteip={}`
+  * On Linux this is: `iptables -A INPUT -s {} -j DROP -m comment --comment "GSDDDOSS blocked"`
+* [CSF+LFD (single)](https://configserver.com/configserver-security-and-firewall/) example: `csf -d {} "GSDDDOSS blocked"`
+* [CSF+LFD (cluster)](https://configserver.com/configserver-security-and-firewall/) example: `csf -cd {} "GSDDDOSS blocked"`
+* [IPSET](https://ipset.netfilter.org/) example: `add blocklist {} comment "GSDDDOSS blocked"` *(Replace "blocklist" with your intended set name).*
+* [UFW](https://wiki.ubuntu.com/UncomplicatedFirewall?action=show&redirect=UbuntuFirewall) example: `ufw deny from {} to any`
+
+### `windows_rule_ip_grouped`
+
+Default: `True`
+
+For Windows systems only: Group all blocked IP addresses into a single rule.
+
+Turn this off if you find that you're exhausting the limitation of IP addresses in a single firewall rule. This is typically 1000, though increased to 10000 on Windows 11 and Windows Server 2022.
+
+Bare in mind the alternative, when this is off, means that your firewall rule set will grow to many thousands. You can clear all of these in one go with this command:
+
+```batch
+netsh advfirewall firewall del rule name="GSDDDOSS blocked" dir=in
 ```
+
+## Installation
+
+Download & run the script. It will automatically start listening on UDP port **8008** or the port you configured above.
+
+In your game server's "server.cfg" file (or alternative `servercfgfile`) add the line `logaddress_add 127.0.0.1 8008`. (Be sure to replace in the listener address/port you configured above.) Then apply this by either restarting the server, restarting/changing map, or execute the same line in the server console (either directly or via RCON).
+
+To terminate the program either send CTRL+C (on Linux) or CTRL+BREAK (on Windows). **CTRL+C will not work on Windows currently.**
+
+## Continuous running
+
+Ideally you want to run the Python command in a loop or as a service in case it crashes or closes unexpectedly.
+
+### Windows
+
+To have this process running continuously in the background you could create a Batch script file of which loops itself after this tool closes.
+
+Make a new ".bat" or ".cmd" file containing:
+
+```batch
 @echo off
 title GSDDDOSS
 :watch
 python gsdddoss.py
 goto watch
 ```
-After that make a shortcut to the bat file, and go to the shortcut properties.
 
-In the shortcut properties find the button labeled "Advanced" and tick off "Run as administrator".
+(This assumes `python` is available to the system's "PATH" environment variable.)
 
-Then you just run the shortcut and press yes during UAC
+After that make a shortcut to the script file, and go to the shortcut properties. In the shortcut properties find the button labelled "Advanced" and tick off "Run as administrator". Then you just run the shortcut and press yes at the UAC.
+
+You may prefer to use a service tool such as [FireDaemon Pro](https://www.firedaemon.com/firedaemon-pro).
+
+### Linux
+
+We'd suggest using `screen` or `tmux` on Linux.
--- gsdddoss.py
+++ gsdddoss.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, signal
 import datetime
 
 isLinux = False
@@ -9,6 +9,19 @@ import re
 import subprocess
 import json
 import socket
+
+
+
+isRunning = False
+
+def signal_handler(signal, frame):
+	print("\nProgram exiting gracefully.")
+	isRunning = False
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+
 
 if not isLinux:
 	import ctypes
@@ -28,23 +41,45 @@ else:
 
 
 if not is_admin():
-	print("This script requires python to be run as administrator!")
-	print("Otherwise no changes can be made to the firewall.")
-	sys.exit(0)
+	print("Error: This script must be run with administrative / super user privileges.")
+	print("(This is necessary to block IP addresses with your system's firewall.)")
+	sys.exit(1)
 
-if not os.path.isfile("ips.json"):
-	print("Missing .json file!\n")
-	print("Please create a file named \"ips.json\" in the script's directory")
-	print("with the contents: {\"banned\": []}")
-	sys.exit(0)
 
-def config_save(data, conf):
-	with open(conf+".json", "w", encoding="utf-8") as f:
+
+
+def config_save(data, name):
+	with open(name + ".json", "w", encoding="utf-8") as f:
 		json.dump(data, f)
 
-def config_load(conf):
-	with open(conf+".json", "r", encoding="utf-8") as f:
+def config_load(name):
+	with open(name + ".json", "r", encoding="utf-8") as f:
 		return json.load(f)
+
+
+
+if not os.path.isfile("config.json"):
+	print("Warning: Configuration file not found. (A default one will be created.)\n")
+	config = {}
+	config["listener_addr"] = "127.0.0.1"
+	config["listener_port"] = 8008
+	config["command_add_block"] = ""
+	config["windows_rule_ip_grouped"] = True
+	config_save(config, "config")
+
+config = config_load("config")
+
+
+
+if not os.path.isfile("blocked.json"):
+	print("Warning: Blocked list file not found. (A default one will be created.)\n")
+	blocked = {}
+	blocked["ips"] = {}
+	config_save(blocked, "blocked")
+
+blocked = config_load("blocked")
+
+
 
 class badip:
 	def __init__(self, ip, port):
@@ -53,29 +88,44 @@ class badip:
 
 def regex(incoming, pattern):
 	regsearch = re.search(r"{}".format(pattern), incoming, re.I)
-	if(regsearch):
+	if (regsearch):
 		return badip(regsearch.group(1), regsearch.group(2))
-	else:
-		return None
+	return None
 
 def getall(ip):
 	ret = ip
-	for storedip in main_config["banned"]:
-		ret = ret+",{}".format(storedip)
+	for blockedIP in blocked["ips"]:
+		ret = ret + ",{}".format(blockedIP)
 
 	return ret
 
 def blockip(ip): #Make one rule per IP, used to group IPs in windows but it quickly hit the firewall rule limitation.
-	if isLinux:
-		subprocess.call("iptables -A INPUT -s {} -j DROP".format(ip), shell=True)
+	if (ip == "127.0.0.1" or ip == "::1"):
+		print("Warning: Not blocking localhost IP address.")
+		return
+
+	blocked["ips"].append(ip)
+	config_save(blocked, "blocked")
+
+	if config["command_add_block"]:
+		firewallRuleAddCommand = subprocess.call(config["command_add_block"].replace("{ip}", ip), shell=True)
+	elif isLinux:
+		firewallRuleAddCommand = subprocess.call("iptables -A INPUT -s {} -j DROP -m comment --comment \"GSDDDOSS blocked\"".format(ip), shell=True)
 	else:
-		subprocess.call("netsh advfirewall firewall add rule name=\"Blocked IP\" dir=in interface=any action=block remoteip={}".format(ip), shell=True)
+		if config["windows_rule_ip_grouped"]:
+			firewallRuleCheckCommand = subprocess.call("netsh advfirewall firewall show rule name=\"GSDDDOSS blocked\" dir=in", shell=True)
+			if (firewallRuleCheckCommand.returncode != 0):
+				firewallRuleAddCommand = subprocess.call("netsh advfirewall firewall add rule name=\"GSDDDOSS blocked\" dir=in action=block description=\"IP addresses that have been caught by the GSDDDOSS tool.\" enable=yes profile=any interface=any remoteip=\"{}\"".format(",".join(blocked["ips"])), shell=True)
+			else:
+				firewallRuleAddCommand = subprocess.call("netsh advfirewall firewall set rule name=\"GSDDDOSS blocked\" dir=in new remoteip=\"{}\"".format(",".join(blocked["ips"])), shell=True)
+		else:
+			firewallRuleAddCommand = subprocess.call("netsh advfirewall firewall add rule name=\"GSDDDOSS blocked\" dir=in action=block description=\"IP address that has been caught by the GSDDDOSS tool.\" enable=yes profile=any interface=any remoteip=\"{}\"".format(ip), shell=True)
 
-	main_config["banned"].append(ip)
-	print("Blocked ip: "+ip)
-	config_save(main_config, "ips")
+	if (firewallRuleAddCommand.returncode != 0):
+		print("Error: Failed to block IP address \"{}\": Code {}".format(ip, firewallRuleAddCommand.returncode))
+	else:
+		print("Blocked IP address \"{}\".".format(ip))
 
-main_config = config_load("ips")
 iplist = {}
 hotlist = {}
 
@@ -83,37 +133,39 @@ def count_ports(input, match):
 	ret = 0
 	for inp in input:
 		if inp == match:
-			ret = ret+1
-
+			ret = ret + 1
 	return ret
 
-#hacky udp loop
-def udp_server(host="127.0.0.1", port=8008):
+def log_receiver(host=config["listener_addr"], port=config["listener_port"]):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	print("Udp started: {}:{}".format(host, port))
-	print("INFINITE LOOP; ^C WILL MOST LIKELY NOT WORK!")
+	print("GSDDDOSS log receiver started, listening on UDP socket at \"{}:{}\".".format(host, port))
 	s.bind((host,port))
-	while True:
-		(data, addr) = s.recvfrom(128*1024)
-		yield data[4:-1].decode("utf-8") # has garbag header + ending
+	isRunning = True
+	while isRunning == True:
+		(data, addr) = s.recvfrom(128 * 1024)
+		if not data:
+			break
+		yield data[4:-1].decode("utf-8") # has garbage header + ending
+	s.close()
 
-for data in udp_server():
-	ret = regex(data, "([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):([0-9]+)\swas\sblocked\sfor\sexceeding\srate\slimits$")
-	if(ret != None):
-		if ret.ip not in main_config["banned"]:
+for data in log_receiver():
+	# Rule: A2S abuse / Reflected DDoS
+	ret = regex(data, "([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+):([0-9]+)\\swas\\sblocked\\sfor\\sexceeding\\srate\\slimits$")
+	if (ret != None):
+		if ret.ip not in blocked["ips"]:
 			if ret.ip in iplist:
 				iplist[ret.ip].append(ret.port)
 				counted = count_ports(iplist[ret.ip], ret.port)
-				if(counted >= 3):
-					print("Found a naughty ip: {} (rate limit, repeating port was: {})".format(ret.ip, ret.port))
+				if (counted >= 3):
+					print("Found a naughty IP address: {} (rate limit, repeating port was: {})".format(ret.ip, ret.port))
 					blockip(ret.ip)
 					if ret.ip in hotlist:
 						hotlist.pop(ret.ip)
 
 					iplist.pop(ret.ip)
 
-				if(counted <= 1 and len(iplist[ret.ip]) > 12):
+				if (counted <= 1 and len(iplist[ret.ip]) > 12):
 					print("Popping: {} (most likely valid person spamming browser refresh)".format(ret.ip))
 					future = datetime.datetime.utcnow() + datetime.timedelta(seconds=240)
 					future = future.replace(tzinfo=datetime.timezone.utc).timestamp()
@@ -123,11 +175,11 @@ for data in udp_server():
 						timenow = datetime.datetime.utcnow()
 						for hottime in hotlist[ret.ip][:]:
 							timethen = datetime.datetime.utcfromtimestamp(hottime)
-							if(timethen < timenow):
+							if (timethen < timenow):
 								hotlist.remove(hottime)
 
 						if len(hotlist[ret.ip]) > 3:
-							print("Found a naughty ip: {} (rate limit, port blasting)".format(ret.ip))
+							print("Found a naughty IP address: {} (rate limit, port blasting)".format(ret.ip))
 							hotlist.pop(ret.ip)
 							blockip(ret.ip)
 					else:
@@ -139,14 +191,16 @@ for data in udp_server():
 				iplist[ret.ip] = []
 				iplist[ret.ip].append(ret.port)
 
-	ret = regex(data, "^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):([0-9]+)\stried\sto\ssend\ssplit\spacket")
-	if(ret != None):
-		if ret.ip not in main_config["banned"]:
-			print("Found extremely naughty ip: {} (SPLIT PACKET!)".format(ret.ip))
+	# Rule: Split packet
+	ret = regex(data, "^([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+):([0-9]+)\\stried\\sto\\ssend\\ssplit\\spacket")
+	if (ret != None):
+		if ret.ip not in blocked["ips"]:
+			print("Found extremely naughty IP address: {} (SPLIT PACKET!)".format(ret.ip))
 			blockip(ret.ip)
 
-	ret = regex(data, "Bad\sRcon:\s(?:.*)\sfrom\s\"([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):([0-9]+)\"")
-	if(ret != None):
-		if ret.ip not in main_config["banned"]:
-			print("Found semi-naughty ip: {} (bad rcon)".format(ret.ip))
+	# Rule: Bad RCON attempt
+	ret = regex(data, "Bad\\sRcon:\\s(?:.*)\\sfrom\\s\"([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+):([0-9]+)\"")
+	if (ret != None):
+		if ret.ip not in blocked["ips"]:
+			print("Found semi-naughty IP address: {} (bad RCON)".format(ret.ip))
 			blockip(ret.ip)
